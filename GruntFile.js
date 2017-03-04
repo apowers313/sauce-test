cd5b02b1f594cd44fc446567db1f32d14f398e01
Adam Powers <apowers@ato.ms>
2017-03-03 21:10:34 -0800

    fixing URL path

--- GruntFile.js
+++ GruntFile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
             all: {
                 options: {
                     urls: [
-                        'http://127.0.0.1:9999/index.html'
+                        'http://127.0.0.1:9999/test/test.html'
                     ],
                     browsers: browsers,
                     build: process.env.TRAVIS_JOB_ID,
